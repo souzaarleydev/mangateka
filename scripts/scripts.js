ebf735c7912b934f11bb5135a541a5cbd71b5288
souzaarleydev <arleysouzadev@gmail.com>
2024-09-27 11:21:12 -0300

    dia 01
    projeto mangateka dia 01.

---
+++ scripts/scripts.js
@@ -0,0 +1,19 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 3,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+});
